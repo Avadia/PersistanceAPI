64b3ed1dc6b57f15fa4978bf737bf35e71693bcc
MisterSatch <jrevet@free.fr>
2016-01-30 22:34:20 +0100

    Ajout de toutes les stats et de la stat globale

--- src/main/java/com/samagames/persistanceapi/datamanager/StatisticsManager.java
+++ src/main/java/com/samagames/persistanceapi/datamanager/StatisticsManager.java
@@ -18,11 +18,13 @@ package com.samagames.persistanceapi.datamanager;
 import com.samagames.persistanceapi.beans.PlayerBean;
 import com.samagames.persistanceapi.beans.PlayerStatisticsBean;
 import com.samagames.persistanceapi.beans.aggregationbean.*;
+import com.samagames.persistanceapi.utils.Transcoder;
+import com.sun.tools.corba.se.idl.constExpr.Times;
+
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.UUID;
+
 
 public class StatisticsManager
 {
@@ -48,13 +50,23 @@ public class StatisticsManager
 
             // Query construction
             String sql = "";
-            sql += "";
+            sql += "select (HEX(uuid)) as uuid, deaths, kills, played_games, wins, creation_date, update_date from dimensions_stats";
+            sql += " where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
 
             // Execute the query
-            resultset = statement.executeQuery(sql);
+            resultset = statement.executeQuery(sql); // TODO check if no results
 
             // Manage the result in a bean
             resultset.next();
+            String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
+            UUID uuid = UUID.fromString(playerUuid);
+            int deaths = resultset.getInt("deaths");
+            int kills = resultset.getInt("kills");
+            int playedGames = resultset.getInt("played_games");
+            int wins = resultset.getInt("wins");
+            Timestamp creationDate = resultset.getTimestamp("creation_date");
+            Timestamp updateDate = resultset.getTimestamp("update_date");
+            dimensionStats = new DimensionStatisticsBean(uuid, deaths, kills, playedGames, wins, creationDate, updateDate);
         }
         catch(SQLException e)
         {
@@ -79,13 +91,25 @@ public class StatisticsManager
 
             // Query construction
             String sql = "";
-            sql += "";
+            sql += "select (HEX(uuid)) as uuid, deaths, elo, kills, played_games, powerup_taken, wins, creation_date, update_date from herobattle_stats";
+            sql += " where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
 
             // Execute the query
-            resultset = statement.executeQuery(sql);
+            resultset = statement.executeQuery(sql); // TODO check if no results
 
             // Manage the result in a bean
             resultset.next();
+            String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
+            UUID uuid = UUID.fromString(playerUuid);
+            int deaths = resultset.getInt("deaths");
+            int elo = resultset.getInt("elo");
+            int kills = resultset.getInt("kills");
+            int playedGames = resultset.getInt("played_games");
+            int powerupTaken = resultset.getInt("powerup_taken");
+            int wins = resultset.getInt("wins");
+            Timestamp creationDate = resultset.getTimestamp("creation_date");
+            Timestamp updateDate = resultset.getTimestamp("update_date");
+            heroBattleStats = new HeroBattleStatisticsBean(uuid, deaths, elo, kills, playedGames, powerupTaken, wins, creationDate, updateDate);
         }
         catch(SQLException e)
         {
@@ -110,13 +134,21 @@ public class StatisticsManager
 
             // Query construction
             String sql = "";
-            sql += "";
+            sql += "select (HEX(uuid)) as uuid, mehs, woots, creation_date, update_date from jukebox_stats";
+            sql +=" where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
 
             // Execute the query
-            resultset = statement.executeQuery(sql);
+            resultset = statement.executeQuery(sql); // TODO check if no results
 
             // Manage the result in a bean
             resultset.next();
+            String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
+            UUID uuid = UUID.fromString(playerUuid);
+            int mehs = resultset.getInt("mehs");
+            int woots = resultset.getInt("woots");
+            Timestamp creationDate = resultset.getTimestamp("creation_date");
+            Timestamp updateDate = resultset.getTimestamp("update_date");
+            jukeBoxStats = new JukeBoxStatisticsBean(uuid, mehs, woots, creationDate, updateDate);
         }
         catch(SQLException e)
         {
@@ -141,13 +173,23 @@ public class StatisticsManager
 
             // Query construction
             String sql = "";
-            sql += "";
+            sql += "select (HEX(uuid)) as uuid, deaths, kills, played_games, wins, creation_date, update_date from quake_stats";
+            sql +=" where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
 
             // Execute the query
-            resultset = statement.executeQuery(sql);
+            resultset = statement.executeQuery(sql); // TODO check if no results
 
             // Manage the result in a bean
             resultset.next();
+            String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
+            UUID uuid = UUID.fromString(playerUuid);
+            int deaths = resultset.getInt("deaths");
+            int kills = resultset.getInt("kills");
+            int playedGames = resultset.getInt("played_games");
+            int wins =  resultset.getInt("wins");
+            Timestamp creationDate = resultset.getTimestamp("creation_date");
+            Timestamp updateDate = resultset.getTimestamp("update_date");
+            quakeStats = new QuakeStatisticsBean(uuid, deaths, kills, playedGames, wins, creationDate, updateDate);
         }
         catch(SQLException e)
         {
@@ -172,13 +214,25 @@ public class StatisticsManager
 
             // Query construction
             String sql = "";
-            sql += "";
+            sql += "select (HEX(uuid)) as uuid, damages, deaths, kills, max_damages, played_games, wins, creation_date, update_date from uhcrun_stats";
+            sql += " where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
 
             // Execute the query
-            resultset = statement.executeQuery(sql);
+            resultset = statement.executeQuery(sql); // TODO check if no results
 
             // Manage the result in a bean
             resultset.next();
+            String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
+            UUID uuid = UUID.fromString(playerUuid);
+            int damages = resultset.getInt("damages");
+            int deaths = resultset.getInt("deaths");
+            int kills = resultset.getInt("kills");
+            int maxDamages = resultset.getInt("max_damages");
+            int playedGames = resultset.getInt("played_games");
+            int wins = resultset.getInt("wins");
+            Timestamp creationDate = resultset.getTimestamp("creation_date");
+            Timestamp updateDate = resultset.getTimestamp("update_date");
+            uhcRunStats = new UHCRunStatisticsBean(uuid, damages, deaths, kills, maxDamages, playedGames, wins, creationDate, updateDate);
         }
         catch(SQLException e)
         {
@@ -203,13 +257,25 @@ public class StatisticsManager
 
             // Query construction
             String sql = "";
-            sql += "";
+            sql += "select (HEX(uuid)) as uuid, blocks, grenades, kills, played_games, tnt_launched, wins, creation_date, update_date from uppervoid_stats";
+            sql += " where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
 
             // Execute the query
-            resultset = statement.executeQuery(sql);
+            resultset = statement.executeQuery(sql); // TODO check if no results
 
             // Manage the result in a bean
             resultset.next();
+            String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
+            UUID uuid = UUID.fromString(playerUuid);
+            int blocks = resultset.getInt("blocks");
+            int grenades = resultset.getInt("grenades");
+            int kills = resultset.getInt("kills");
+            int playedGames = resultset.getInt("played_games");
+            int tntLaunched = resultset.getInt("tnt_launched");
+            int wins = resultset.getInt("wins");
+            Timestamp creationDate = resultset.getTimestamp("creation_date");
+            Timestamp updateDate = resultset.getTimestamp("update_date");
+            uppervoidStats = new UppervoidStatistics(uuid, blocks, grenades, kills, playedGames, tntLaunched, wins, creationDate, updateDate);
         }
         catch(SQLException e)
         {
