3598ac27a3f3672e524ba7fc04ba7564f1afd606
MisterSatch <jrevet@free.fr>
2016-01-31 18:00:53 +0100

    Modification en classe d'aggrégation

--- src/main/java/com/samagames/persistanceapi/datamanager/StatisticsManager.java
+++ src/main/java/com/samagames/persistanceapi/datamanager/StatisticsManager.java
@@ -18,376 +18,43 @@ package com.samagames.persistanceapi.datamanager;
 import com.samagames.persistanceapi.beans.PlayerBean;
 import com.samagames.persistanceapi.beans.PlayerStatisticsBean;
 import com.samagames.persistanceapi.beans.aggregationbean.*;
-import com.samagames.persistanceapi.utils.Transcoder;
-import com.sun.tools.corba.se.idl.constExpr.Times;
-
+import com.samagames.persistanceapi.datamanager.aggregationmanager.*;
 import javax.sql.DataSource;
-import java.sql.*;
-import java.util.UUID;
-
 
 public class StatisticsManager
 {
     // Defines
-    Connection connection = null;
-    Statement statement = null;
-    ResultSet resultset = null;
-    DimensionStatisticsBean dimensionStats = null;
-    HeroBattleStatisticsBean heroBattleStats = null;
-    JukeBoxStatisticsBean jukeBoxStats = null;
-    QuakeStatisticsBean quakeStats = null;
-    UHCRunStatisticsBean uhcRunStats = null;
-    UppervoidStatistics uppervoidStats = null;
+    public DimensionStatisticsManager dimensionStatsManager;
+    public HeroBattleStatisticsManager heroBattleStatsManager;
+    public JukeBoxStatisticsManager jukeBoxStatsManager;
+    public QuakeStatisticsManager quakeStatsManager;
+    public UHCRunStatisticsManager uhcRunStatsManager;
+    public UpperVoidStatisticsManager upperVoidStatsManager;
 
-    // Get Dimension player statistics
-    public DimensionStatisticsBean getDimensionStatistics(PlayerBean player, DataSource dataSource)
+    // Constructor
+    public StatisticsManager()
     {
-        try
-        {
-            // Set connection
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-
-            // Query construction
-            String sql = "";
-            sql += "select (HEX(uuid)) as uuid, deaths, kills, played_games, wins, creation_date, update_date from dimensions_stats";
-            sql += " where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
-
-            // Execute the query
-            resultset = statement.executeQuery(sql);
-
-            // Manage the result in a bean
-            if(resultset.next())
-            {
-                // There's a result
-                String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
-                UUID uuid = UUID.fromString(playerUuid);
-                int deaths = resultset.getInt("deaths");
-                int kills = resultset.getInt("kills");
-                int playedGames = resultset.getInt("played_games");
-                int wins = resultset.getInt("wins");
-                Timestamp creationDate = resultset.getTimestamp("creation_date");
-                Timestamp updateDate = resultset.getTimestamp("update_date");
-                dimensionStats = new DimensionStatisticsBean(uuid, deaths, kills, playedGames, wins, creationDate, updateDate);
-            }
-        }
-        catch(SQLException exception)
-        {
-            exception.printStackTrace();
-        }
-        finally
-        {
-            // Close the query environment in order to prevent leaks
-            close();
-        }
-        return dimensionStats;
-    }
-
-    // Get HeroBattle palyer statistics
-    public HeroBattleStatisticsBean getHeroBattleStatistics(PlayerBean player, DataSource dataSource)
-    {
-        try
-        {
-            // Set connection
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-
-            // Query construction
-            String sql = "";
-            sql += "select (HEX(uuid)) as uuid, deaths, elo, kills, played_games, powerup_taken, wins, creation_date, update_date from herobattle_stats";
-            sql += " where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
-
-            // Execute the query
-            resultset = statement.executeQuery(sql);
-
-            // Manage the result in a bean
-            if (resultset.next())
-            {
-                // There's a result
-                String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
-                UUID uuid = UUID.fromString(playerUuid);
-                int deaths = resultset.getInt("deaths");
-                int elo = resultset.getInt("elo");
-                int kills = resultset.getInt("kills");
-                int playedGames = resultset.getInt("played_games");
-                int powerupTaken = resultset.getInt("powerup_taken");
-                int wins = resultset.getInt("wins");
-                Timestamp creationDate = resultset.getTimestamp("creation_date");
-                Timestamp updateDate = resultset.getTimestamp("update_date");
-                heroBattleStats = new HeroBattleStatisticsBean(uuid, deaths, elo, kills, playedGames, powerupTaken, wins, creationDate, updateDate);
-            }
-        }
-        catch(SQLException exception)
-        {
-            exception.printStackTrace();
-        }
-        finally
-        {
-            // Close the query environment in order to prevent leaks
-            close();
-        }
-        return heroBattleStats;
-    }
-
-    // Get JukeBox player statistics
-    public JukeBoxStatisticsBean getJukeBoxStatistics(PlayerBean player, DataSource dataSource)
-    {
-        try
-        {
-            // Set connection
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-
-            // Query construction
-            String sql = "";
-            sql += "select (HEX(uuid)) as uuid, mehs, woots, creation_date, update_date from jukebox_stats";
-            sql +=" where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
-
-            // Execute the query
-            resultset = statement.executeQuery(sql);
-
-            // Manage the result in a bean
-            if (resultset.next())
-            {
-                // There's a result
-                String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
-                UUID uuid = UUID.fromString(playerUuid);
-                int mehs = resultset.getInt("mehs");
-                int woots = resultset.getInt("woots");
-                Timestamp creationDate = resultset.getTimestamp("creation_date");
-                Timestamp updateDate = resultset.getTimestamp("update_date");
-                jukeBoxStats = new JukeBoxStatisticsBean(uuid, mehs, woots, creationDate, updateDate);
-            }
-        }
-        catch(SQLException exception)
-        {
-            exception.printStackTrace();
-        }
-        finally
-        {
-            // Close the query environment in order to prevent leaks
-            close();
-        }
-        return jukeBoxStats;
-    }
-
-    // Get Quake player statistics
-    public QuakeStatisticsBean getQuakeStatistics(PlayerBean player, DataSource dataSource)
-    {
-        try
-        {
-            // Set connection
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-
-            // Query construction
-            String sql = "";
-            sql += "select (HEX(uuid)) as uuid, deaths, kills, played_games, wins, creation_date, update_date from quake_stats";
-            sql +=" where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
-
-            // Execute the query
-            resultset = statement.executeQuery(sql);
-
-            // Manage the result in a bean
-            if (resultset.next())
-            {
-                // There's a result
-                String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
-                UUID uuid = UUID.fromString(playerUuid);
-                int deaths = resultset.getInt("deaths");
-                int kills = resultset.getInt("kills");
-                int playedGames = resultset.getInt("played_games");
-                int wins = resultset.getInt("wins");
-                Timestamp creationDate = resultset.getTimestamp("creation_date");
-                Timestamp updateDate = resultset.getTimestamp("update_date");
-                quakeStats = new QuakeStatisticsBean(uuid, deaths, kills, playedGames, wins, creationDate, updateDate);
-            }
-        }
-        catch(SQLException exception)
-        {
-            exception.printStackTrace();
-        }
-        finally
-        {
-            // Close the query environment in order to prevent leaks
-            close();
-        }
-        return quakeStats;
-    }
-
-    // Get UHCRun player statistics
-    public UHCRunStatisticsBean getUHCRunStatistics(PlayerBean player, DataSource dataSource)
-    {
-        try
-        {
-            // Set connection
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-
-            // Query construction
-            String sql = "";
-            sql += "select (HEX(uuid)) as uuid, damages, deaths, kills, max_damages, played_games, wins, creation_date, update_date from uhcrun_stats";
-            sql += " where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
-
-            // Execute the query
-            resultset = statement.executeQuery(sql);
-
-            // Manage the result in a bean
-            if (resultset.next())
-            {
-                // There's a result
-                String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
-                UUID uuid = UUID.fromString(playerUuid);
-                int damages = resultset.getInt("damages");
-                int deaths = resultset.getInt("deaths");
-                int kills = resultset.getInt("kills");
-                int maxDamages = resultset.getInt("max_damages");
-                int playedGames = resultset.getInt("played_games");
-                int wins = resultset.getInt("wins");
-                Timestamp creationDate = resultset.getTimestamp("creation_date");
-                Timestamp updateDate = resultset.getTimestamp("update_date");
-                uhcRunStats = new UHCRunStatisticsBean(uuid, damages, deaths, kills, maxDamages, playedGames, wins, creationDate, updateDate);
-            }
-        }
-        catch(SQLException exception)
-        {
-            exception.printStackTrace();
-        }
-        finally
-        {
-            // Close the query environment in order to prevent leaks
-            close();
-        }
-        return uhcRunStats;
-    }
-
-    // Get UpperVoid player statistics
-    public UppervoidStatistics getUpperVoidStatistics(PlayerBean player, DataSource dataSource)
-    {
-        try
-        {
-            // Set connection
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-
-            // Query construction
-            String sql = "";
-            sql += "select (HEX(uuid)) as uuid, blocks, grenades, kills, played_games, tnt_launched, wins, creation_date, update_date from uppervoid_stats";
-            sql += " where uuid=(UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"'))";
-
-            // Execute the query
-            resultset = statement.executeQuery(sql);
-
-            // Manage the result in a bean
-            if (resultset.next())
-            {
-                // There's a result
-                String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
-                UUID uuid = UUID.fromString(playerUuid);
-                int blocks = resultset.getInt("blocks");
-                int grenades = resultset.getInt("grenades");
-                int kills = resultset.getInt("kills");
-                int playedGames = resultset.getInt("played_games");
-                int tntLaunched = resultset.getInt("tnt_launched");
-                int wins = resultset.getInt("wins");
-                Timestamp creationDate = resultset.getTimestamp("creation_date");
-                Timestamp updateDate = resultset.getTimestamp("update_date");
-                uppervoidStats = new UppervoidStatistics(uuid, blocks, grenades, kills, playedGames, tntLaunched, wins, creationDate, updateDate);
-            }
-        }
-        catch(SQLException exception)
-        {
-            exception.printStackTrace();
-        }
-        finally
-        {
-            // Close the query environment in order to prevent leaks
-            close();
-        }
-        return uppervoidStats;
+        this.dimensionStatsManager = new DimensionStatisticsManager();
+        this.heroBattleStatsManager = new HeroBattleStatisticsManager();
+        this.jukeBoxStatsManager = new JukeBoxStatisticsManager();
+        this.quakeStatsManager = new QuakeStatisticsManager();
+        this.uhcRunStatsManager = new UHCRunStatisticsManager();
+        this.upperVoidStatsManager = new UpperVoidStatisticsManager();
     }
 
     // Get all player statistics
     public PlayerStatisticsBean getAllPlayerStatistics(PlayerBean player, DataSource dataSource)
     {
         // Get the different statistics bean
-        DimensionStatisticsBean dimensionStats = this.getDimensionStatistics(player, dataSource);
-        HeroBattleStatisticsBean heroBattleStats = this.getHeroBattleStatistics(player, dataSource);
-        JukeBoxStatisticsBean jukeBoxStats = this.getJukeBoxStatistics(player, dataSource);
-        QuakeStatisticsBean quakeStats = this.getQuakeStatistics(player, dataSource);
-        UHCRunStatisticsBean uhcRunStatcs = this.getUHCRunStatistics(player, dataSource);
-        UppervoidStatistics upperVoidStats = this.getUpperVoidStatistics(player, dataSource);
+        DimensionStatisticsBean dimensionStats = this.dimensionStatsManager.getDimensionStatistics(player, dataSource);
+        HeroBattleStatisticsBean heroBattleStats = this.heroBattleStatsManager.getHeroBattleStatistics(player, dataSource);
+        JukeBoxStatisticsBean jukeBoxStats = this.jukeBoxStatsManager.getJukeBoxStatistics(player, dataSource);
+        QuakeStatisticsBean quakeStats = this.quakeStatsManager.getQuakeStatistics(player, dataSource);
+        UHCRunStatisticsBean uhcRunStatcs = this.uhcRunStatsManager.getUHCRunStatistics(player, dataSource);
+        UppervoidStatistics upperVoidStats = this.upperVoidStatsManager.getUpperVoidStatistics(player, dataSource);
+
         // Create the aggregation of different statistics bean
         PlayerStatisticsBean playerStatisticsBean = new PlayerStatisticsBean(dimensionStats, heroBattleStats, jukeBoxStats, quakeStats, uhcRunStatcs, upperVoidStats);
         return playerStatisticsBean;
     }
-
-    // Update Dimension player statistics // FIXME WIP
-    public void updateDimensionStatistics(PlayerBean player, DataSource dataSource)
-    {
-        try
-        {
-            // Set connection
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-
-            // Check if a record exists
-            if (this.getDimensionStatistics(player, dataSource) == null)
-            {
-                // Query construction for create
-                String sql = "";
-                sql += "";
-
-                // Execute the query
-                resultset = statement.executeQuery(sql);
-            }
-            else
-            {
-                // Query construction for update
-                String sql = "";
-                sql += "";
-
-                // Execute the query
-                statement.executeUpdate(sql);
-            }
-        }
-        catch(SQLException exception)
-        {
-            exception.printStackTrace();
-        }
-        finally
-        {
-            // Close the query environment in order to prevent leaks
-            close();
-        }
-    }
-
-    // Close the connection
-    public void close()
-    {
-        // Close the query environment in order to prevent leaks
-        try
-        {
-            if (resultset != null)
-            {
-                // Close the resulset
-                resultset.close();
-            }
-            if (statement != null)
-            {
-                // Close the statement
-                statement.close();
-            }
-            if (connection != null)
-            {
-                // Close the connection
-                connection.close();
-            }
-        }
-        catch(Exception exception)
-        {
-            exception.printStackTrace();
-        }
-    }
-
 }
