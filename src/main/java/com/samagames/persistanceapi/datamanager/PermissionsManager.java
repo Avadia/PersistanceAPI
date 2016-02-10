625794cbf133f67ad8ee0dca2f51d9edfb63820a
TheGreatAncien <jrevet@free.fr>
2016-02-10 14:33:23 +0100

    Création du manager de permission

---
+++ src/main/java/com/samagames/persistanceapi/datamanager/PermissionsManager.java
@@ -0,0 +1,30 @@
+/*
+===============================================================
+   _____                       ______
+  / ___/____ _____ ___  ____ _/ ____/___ _____ ___  ___  _____
+  \__ \/ __ `/ __ `__ \/ __ `/ / __/ __ `/ __ `__ \/ _ \/ ___/
+ ___/ / /_/ / / / / / / /_/ / /_/ / /_/ / / / / / /  __(__  )
+/____/\__,_/_/ /_/ /_/\__,_/\____/\__,_/_/ /_/ /_/\___/____/
+
+===============================================================
+  Persistance API
+  Copyright (c) for SamaGames, all right reserved
+  By MisterSatch, January 2016
+===============================================================
+*/
+
+package com.samagames.persistanceapi.datamanager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class PermissionsManager
+{
+    // Defines
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultset = null;
+
+
+}
