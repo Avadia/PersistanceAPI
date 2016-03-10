87623d36a5ff86d264fcf1da079616f6e03ba8a5
TheGreatAncien <jrevet@free.fr>
2016-03-10 16:34:42 +0100

    Creation de managers composites pour les permissions

---
+++ src/main/java/net/samagames/persistanceapi/datamanager/aggregationmanager/permissions/ModerationPermissionManager.java
@@ -0,0 +1,30 @@
+/*
+===============================================================
+   _____                       ______
+  / ___/____ _____ ___  ____ _/ ____/___ _____ ___  ___  _____
+  \__ \/ __ `/ __ `__ \/ __ `/ / __/ __ `/ __ `__ \/ _ \/ ___/
+ ___/ / /_/ / / / / / / /_/ / /_/ / /_/ / / / / / /  __(__  )
+/____/\__,_/_/ /_/ /_/\__,_/\____/\__,_/_/ /_/ /_/\___/____/
+
+===============================================================
+  Persistance API
+  Copyright (c) for SamaGames, all right reserved
+  By MisterSatch, January 2016
+===============================================================
+*/
+
+package net.samagames.persistanceapi.datamanager.aggregationmanager.permissions;
+
+import net.samagames.persistanceapi.beans.PlayerBean;
+import net.samagames.persistanceapi.beans.permissions.ModerationPermissionsBean;
+
+import javax.sql.DataSource;
+
+public class ModerationPermissionManager
+{
+
+    public ModerationPermissionsBean getPermission(PlayerBean player, DataSource dataSource)
+    {
+        return null;
+    }
+}
