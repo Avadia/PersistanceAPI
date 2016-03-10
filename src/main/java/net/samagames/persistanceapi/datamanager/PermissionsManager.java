3659ac0fc999d465942eea0b93402839edcf3007
TheGreatAncien <jrevet@free.fr>
2016-03-10 16:34:18 +0100

    Mise en place du permission manager

--- src/main/java/net/samagames/persistanceapi/datamanager/PermissionsManager.java
+++ src/main/java/net/samagames/persistanceapi/datamanager/PermissionsManager.java
@@ -15,15 +15,52 @@
 
 package net.samagames.persistanceapi.datamanager;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import net.samagames.persistanceapi.beans.PlayerBean;
+import net.samagames.persistanceapi.beans.permissions.*;
+import net.samagames.persistanceapi.datamanager.aggregationmanager.permissions.*;
+import javax.sql.DataSource;
 
 public class PermissionsManager
 {
     // Defines
-    Connection connection = null;
-    Statement statement = null;
-    ResultSet resultset = null;
+    public APIPermissionManager apiPermissionManager;
+    public BukkitPermissionManager bukkitPermissionManager;
+    public BungeeRedisPermissionManager bungeeRedisPermissionManager;
+    public HubPermissionManager hubPermissionManager;
+    public ModerationPermissionManager moderationPermissionManager;
+    public ProxiesPermissionManager proxiesPermissionManager;
+    public StaffPermissionManager staffPermissionManager;
+    public UHCPermissionManager uhcPermissionManager;
 
+    // Constructor
+    public PermissionsManager()
+    {
+        this.apiPermissionManager =  new APIPermissionManager();
+        this.bukkitPermissionManager = new BukkitPermissionManager();
+        this.bungeeRedisPermissionManager = new BungeeRedisPermissionManager();
+        this.hubPermissionManager = new HubPermissionManager();
+        this.moderationPermissionManager = new ModerationPermissionManager();
+        this.proxiesPermissionManager = new ProxiesPermissionManager();
+        this.staffPermissionManager = new StaffPermissionManager();
+        this.uhcPermissionManager = new UHCPermissionManager();
+    }
+
+    // Get all player permissions
+    public PlayerPermissionBean getPlayerPermission(PlayerBean player, DataSource dataSource)
+    {
+        // Get the different permissions bean
+        APIPermissionsBean apiPermissions = this.apiPermissionManager.getPermission(player, dataSource);
+        BukkitPermissionsBean bukkitPermissions = this.bukkitPermissionManager.getPermission(player, dataSource);
+        BungeeRedisPermissionsBean bungeeRedisPermisions = this.bungeeRedisPermissionManager.getPemission(player, dataSource);
+        HubPermissionsBean hubPermissions = this.hubPermissionManager.getPermission(player, dataSource);
+        ModerationPermissionsBean moderationPermissions = this.moderationPermissionManager.getPermission(player, dataSource);
+        ProxiesPermissionsBean proxiesPermissions = this.proxiesPermissionManager.getPermission(player, dataSource);
+        StaffPermissionsBean staffPermissions = this.staffPermissionManager.getPermission(player, dataSource);
+        UHCPermissionsBean uhcPermissions = this.uhcPermissionManager.getPermission(player, dataSource);
+
+        // Create the aggregation of different permissions bean
+        PlayerPermissionBean playerPermissionBean = new PlayerPermissionBean(apiPermissions, bukkitPermissions, bungeeRedisPermisions, hubPermissions, moderationPermissions, proxiesPermissions,
+                staffPermissions, uhcPermissions);
+        return playerPermissionBean;
+    }
 }
