4dade4f046f3e37a19ff087652228a9f88104ac7
TheGreatAncien <jrevet@free.fr>
2016-03-11 15:06:00 +0100

    Création de la requête SQL

--- src/main/java/net/samagames/persistanceapi/datamanager/aggregationmanager/permissions/ModerationPermissionManager.java
+++ src/main/java/net/samagames/persistanceapi/datamanager/aggregationmanager/permissions/ModerationPermissionManager.java
@@ -16,15 +16,95 @@
 package net.samagames.persistanceapi.datamanager.aggregationmanager.permissions;
 
 import net.samagames.persistanceapi.beans.PlayerBean;
+import net.samagames.persistanceapi.beans.permissions.APIPermissionsBean;
 import net.samagames.persistanceapi.beans.permissions.ModerationPermissionsBean;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ModerationPermissionManager
 {
+    // Defines
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultset = null;
+    ModerationPermissionsBean moderationPermissionsBean = null;
 
     public ModerationPermissionsBean getPermission(PlayerBean player, DataSource dataSource)
     {
-        return null;
+        try
+        {
+            // Set connection
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+
+            // Query construction
+            String sql = "select groups_id, mod_ban, mod_tp, mod_kick, mod_pardon, mod_mute_longtime, mod_mute, mod_channel";
+            sql += " from moderation_permissions where group_id=" + player.getGroupId();
+
+            // Execute the query
+            resultset = statement.executeQuery(sql);
+
+            // Manage the result in a bean
+            if(resultset.next())
+            {
+                // There's a result
+                long groupId = resultset.getLong("group_id");
+                boolean modBan = resultset.getBoolean("mod_ban");
+                boolean modTp = resultset.getBoolean("mod_tp");
+                boolean modKick = resultset.getBoolean("mod_kick");
+                boolean modPardon = resultset.getBoolean("mod_pardon");
+                boolean modMuteLongtime = resultset.getBoolean("mod_mute_longtime");
+                boolean modMute = resultset.getBoolean("mod_mute");
+                boolean modChannel = resultset.getBoolean("mod_channel");
+                moderationPermissionsBean = new ModerationPermissionsBean(groupId, modBan, modTp, modKick, modPardon, modMuteLongtime, modMute, modChannel);
+            }
+            else
+            {
+                // If there no dimension stats int the database
+                return null;
+            }
+        }
+        catch(SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            // Close the query environment in order to prevent leaks
+            close();
+        }
+        return moderationPermissionsBean;
+    }
+
+    // Close all connection
+    public void close()
+    {
+        // Close the query environment in order to prevent leaks
+        try
+        {
+            if (resultset != null)
+            {
+                // Close the resulset
+                resultset.close();
+            }
+            if (statement != null)
+            {
+                // Close the statement
+                statement.close();
+            }
+            if (connection != null)
+            {
+                // Close the connection
+                connection.close();
+            }
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
     }
 }
