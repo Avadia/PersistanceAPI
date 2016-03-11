eb9048c89416485d62ad13e115cec6bd55f73caa
TheGreatAncien <jrevet@free.fr>
2016-03-11 16:18:59 +0100

    Correction nom de colonne incorrect

--- src/main/java/net/samagames/persistanceapi/datamanager/aggregationmanager/permissions/HubPermissionManager.java
+++ src/main/java/net/samagames/persistanceapi/datamanager/aggregationmanager/permissions/HubPermissionManager.java
@@ -43,10 +43,10 @@ public class HubPermissionManager
             statement = connection.createStatement();
 
             // Query construction
-            String sql = "select group_id, hub_jukebox_lock, hub_jukebox_next, hub_jukebox_clear, hub_mod_slow, hub_mod_shutup, hub_admin_npc, hub_admin_sign, hub_anguille," +
-                    " hub_jukebox_nbs, hub_admin_evacuate, hub_announce, hub_gadgets_cooldownbypass, hub_gadgets_nuke, hub_jukebox_limitbypass, hub_jukebox_limitstaff," +
-                    " hub_bypassmute, hub_fly, hub_debug_sign";
-            sql += " from hub_permissions where group_id=" + player.getGroupId();
+            String sql = "select groups_id, hub_jukebox_lock, hub_jukebox_next, hub_jukebox_clear, hub_mod_slow, hub_mod_shutup, hub_admin_npc, hub_admin_sign, hub_anguille,";
+            sql += " hub_jukebox_nbs, hub_admin_evacuate, hub_announce, hub_gadgets_cooldownbypass, hub_gadgets_nuke, hub_jukebox_limitbypass, hub_jukebox_limitstaff,";
+            sql += " hub_bypassmute, hub_fly, hub_debug_sign";
+            sql += " from hub_permissions where groups_id=" + player.getGroupId();
 
             // Execute the query
             resultset = statement.executeQuery(sql);
@@ -55,25 +55,25 @@ public class HubPermissionManager
             if(resultset.next())
             {
                 // There's a result
-                long groupId = resultset.getLong("group_id");
-                boolean hubJukeboxLock = resultset.getBoolean("");
-                boolean hubJukeboxNext = resultset.getBoolean("");
-                boolean hubJukeBoxClear = resultset.getBoolean("");
-                boolean hubModSlow = resultset.getBoolean("");
-                boolean hubModShutup = resultset.getBoolean("");
-                boolean hubAdminNpc = resultset.getBoolean("");
-                boolean hubAdminSign = resultset.getBoolean("");
-                boolean hubAnguille = resultset.getBoolean("");
-                boolean hubJukeboxNbs = resultset.getBoolean("");
-                boolean hubAdminEvacuate = resultset.getBoolean("");
-                boolean hubAnnounce = resultset.getBoolean("");
-                boolean hubGadgetsCooldownbypass = resultset.getBoolean("");
-                boolean hubGadgetsNuke = resultset.getBoolean("");
-                boolean hubJukeboxLimitbypass = resultset.getBoolean("");
-                boolean hubJukeboxLimitstaff = resultset.getBoolean("");
-                boolean hubBypassmute = resultset.getBoolean("");
-                boolean hubFly = resultset.getBoolean("");
-                boolean hubDebugSign = resultset.getBoolean("");
+                long groupId = resultset.getLong("groups_id");
+                boolean hubJukeboxLock = resultset.getBoolean("hub_jukebox_lock");
+                boolean hubJukeboxNext = resultset.getBoolean("hub_jukebox_next");
+                boolean hubJukeBoxClear = resultset.getBoolean("hub_jukebox_clear");
+                boolean hubModSlow = resultset.getBoolean("hub_mod_slow");
+                boolean hubModShutup = resultset.getBoolean("hub_mod_shutup");
+                boolean hubAdminNpc = resultset.getBoolean("hub_admin_npc");
+                boolean hubAdminSign = resultset.getBoolean("hub_admin_sign");
+                boolean hubAnguille = resultset.getBoolean("hub_anguille");
+                boolean hubJukeboxNbs = resultset.getBoolean("hub_jukebox_nbs");
+                boolean hubAdminEvacuate = resultset.getBoolean("hub_admin_evacuate");
+                boolean hubAnnounce = resultset.getBoolean("hub_announce");
+                boolean hubGadgetsCooldownbypass = resultset.getBoolean("hub_gadgets_cooldownbypass");
+                boolean hubGadgetsNuke = resultset.getBoolean("hub_gadgets_nuke");
+                boolean hubJukeboxLimitbypass = resultset.getBoolean("hub_jukebox_limitbypass");
+                boolean hubJukeboxLimitstaff = resultset.getBoolean("hub_jukebox_limitstaff");
+                boolean hubBypassmute = resultset.getBoolean("hub_bypassmute");
+                boolean hubFly = resultset.getBoolean("hub_fly");
+                boolean hubDebugSign = resultset.getBoolean("hub_debug_sign");
                 hubPermissionsBean = new HubPermissionsBean(groupId, hubJukeboxLock, hubJukeboxNext, hubJukeBoxClear, hubModSlow, hubModShutup, hubAdminNpc, hubAdminSign, hubAnguille,
                         hubJukeboxNbs, hubAdminEvacuate, hubAnnounce, hubGadgetsCooldownbypass, hubGadgetsNuke, hubJukeboxLimitbypass, hubJukeboxLimitstaff, hubBypassmute, hubFly,
                         hubDebugSign);
