6ab5c4e6c3740e3b2a4576831f874e9dd3a73806
TheGreatAncien <jrevet@free.fr>
2016-03-11 15:17:21 +0100

    Ajout de la requête SQL

--- src/main/java/net/samagames/persistanceapi/datamanager/aggregationmanager/permissions/ProxiesPermissionManager.java
+++ src/main/java/net/samagames/persistanceapi/datamanager/aggregationmanager/permissions/ProxiesPermissionManager.java
@@ -16,15 +16,93 @@
 package net.samagames.persistanceapi.datamanager.aggregationmanager.permissions;
 
 import net.samagames.persistanceapi.beans.PlayerBean;
+import net.samagames.persistanceapi.beans.permissions.APIPermissionsBean;
 import net.samagames.persistanceapi.beans.permissions.ProxiesPermissionsBean;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ProxiesPermissionManager
 {
+    // Defines
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultset = null;
+    ProxiesPermissionsBean proxiesPermissionsBean = null;
 
+    // Get the permissions for proxies
     public ProxiesPermissionsBean getPermission(PlayerBean player, DataSource dataSource)
     {
-        return null;
+        try
+        {
+            // Set connection
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+
+            // Query construction
+            String sql = "select groups_id, proxies_dispatch, proxies_global, proxies_debug, proxies_set_option";
+            sql += " from proxies_permissions where group_id=" + player.getGroupId();
+
+            // Execute the query
+            resultset = statement.executeQuery(sql);
+
+            // Manage the result in a bean
+            if(resultset.next())
+            {
+                // There's a result
+                long groupId = resultset.getLong("group_id");
+                boolean proxiesDispatch = resultset.getBoolean("proxies_dispatch");
+                boolean proxiesGlobal = resultset.getBoolean("proxies_global");
+                boolean proxiesDebug = resultset.getBoolean("proxies_debug");
+                boolean proxiesSetOption = resultset.getBoolean("proxies_set_option");
+                proxiesPermissionsBean = new ProxiesPermissionsBean(groupId, proxiesDispatch, proxiesGlobal, proxiesDebug, proxiesSetOption);
+            }
+            else
+            {
+                // If there no dimension stats int the database
+                return null;
+            }
+        }
+        catch(SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            // Close the query environment in order to prevent leaks
+            close();
+        }
+        return proxiesPermissionsBean;
+    }
+
+    // Close all connection
+    public void close()
+    {
+        // Close the query environment in order to prevent leaks
+        try
+        {
+            if (resultset != null)
+            {
+                // Close the resulset
+                resultset.close();
+            }
+            if (statement != null)
+            {
+                // Close the statement
+                statement.close();
+            }
+            if (connection != null)
+            {
+                // Close the connection
+                connection.close();
+            }
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
     }
 }
