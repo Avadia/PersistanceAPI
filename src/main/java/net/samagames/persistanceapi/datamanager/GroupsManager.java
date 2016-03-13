8dac7c402aa4d5996a229cd2f7eb0e61e857b7b3
Jacky Revet <Jacky@Mac-Pro-de-Jacky.local>
2016-03-13 15:02:49 +0100

    Ajout de la requête SQL de sélection de group

--- src/main/java/net/samagames/persistanceapi/datamanager/GroupsManager.java
+++ src/main/java/net/samagames/persistanceapi/datamanager/GroupsManager.java
@@ -15,7 +15,93 @@
 
 package net.samagames.persistanceapi.datamanager;
 
+import net.samagames.persistanceapi.beans.GroupsBean;
+import net.samagames.persistanceapi.beans.PlayerBean;
+import javax.sql.DataSource;
+import java.sql.*;
+
 public class GroupsManager
 {
+    // Defines
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultset = null;
+    GroupsBean groupsBean = null;
+
+    // Get the permission group for a player
+    public GroupsBean getGroupPlayer(PlayerBean player, DataSource dataSource)
+    {
+        try
+        {
+            // Set connection
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+
+            // Query construction
+            String sql = "";
+            sql += "select group_id, player_name, rank, tag, prefix, suffix, multiplier from groups";
+            sql += " where group_id=" + player.getGroupId();
+
+            // Execute the query
+            resultset = statement.executeQuery(sql);
+
+            // Manage the result in a bean
+            if(resultset.next())
+            {
+                // There's a result
+                long groupId = resultset.getLong("group_id");
+                String playerName = resultset.getString("player_name");
+                int rank = resultset.getInt("rank");
+                String tag = resultset.getString("tag");
+                String prefix = resultset.getString("prefix");
+                String suffix = resultset.getString("suffix");
+                int multiplier = resultset.getInt("multiplier");
+                groupsBean = new GroupsBean(groupId, playerName, rank, tag, prefix, suffix, multiplier);
+            }
+            else
+            {
+                // If there no dimension stats int the database
+                return null;
+            }
+        }
+        catch(SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            // Close the query environment in order to prevent leaks
+            close();
+        }
+        return groupsBean;
+    }
+
+    // Close all connection
+    public void close()
+    {
+        // Close the query environment in order to prevent leaks
+        try
+        {
+            if (resultset != null)
+            {
+                // Close the resulset
+                resultset.close();
+            }
+            if (statement != null)
+            {
+                // Close the statement
+                statement.close();
+            }
+            if (connection != null)
+            {
+                // Close the connection
+                connection.close();
+            }
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
 
 }
