d39ffd028eff08eb104fc6948ff1b61d3149a5f3
Jacky Revet <Jacky@Mac-Pro-de-Jacky.local>
2016-03-13 21:45:11 +0100

    création des requêtes SQL

--- src/main/java/net/samagames/persistanceapi/datamanager/PromotionsManager.java
+++ src/main/java/net/samagames/persistanceapi/datamanager/PromotionsManager.java
@@ -15,5 +15,195 @@
 
 package net.samagames.persistanceapi.datamanager;
 
-public class PromotionsManager {
+import net.samagames.persistanceapi.beans.PromotionsBean;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class PromotionsManager
+{
+    // Defines
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultset = null;
+    ArrayList<PromotionsBean> promotionList = new ArrayList<>(); // Fixme see for concurrency !
+    //List promotionList = Collections.synchronizedList(new ArrayList<PromotionsBean>);
+
+    // Get all the promotions
+    public ArrayList<PromotionsBean> getAllActivePromotions(DataSource dataSource)
+    {
+        try
+        {
+            // Set connection
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+
+            // Query construction
+            String sql = "";
+            sql += "select promotion_id, type_promotion, game, multiplier, message, start_date, end_date from promotions where end_date>now()";
+
+            // Execute the query
+            resultset = statement.executeQuery(sql);
+
+            // Manage the result in a bean
+            while(resultset.next())
+            {
+                // Manage the result in a list of bean
+                long promitionId = resultset.getLong("promotion_id");
+                int typePromotion = resultset.getInt("type_promotion");
+                int game = resultset.getInt("game");
+                int multiplier = resultset.getInt("multiplier");
+                String message = resultset.getString("message");
+                Timestamp startDate = resultset.getTimestamp("start_date");
+                Timestamp endDate = resultset.getTimestamp("end_date");
+                PromotionsBean promotionsBean = new PromotionsBean(promitionId, typePromotion, game, multiplier, message, startDate, endDate);
+                promotionList.add(promotionsBean);
+            }
+            return promotionList;
+        }
+        catch(SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            // Close the query environment in order to prevent leaks
+            close();
+        }
+        return null;
+    }
+
+    // Get a special promotions
+    public ArrayList<PromotionsBean> getPromotion(DataSource dataSource, int typePromotion, int typeGame)
+    {
+        try
+        {
+            // Set connection
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+
+            // Query construction
+            String sql = "";
+            sql += "select promotion_id, type_promotion, game, multiplier, message, start_date, end_date from promotions where end_date>now()";
+            sql += " and type_promotion=" + typePromotion;
+            sql += " and game=" + typeGame;
+
+            // Execute the query
+            resultset = statement.executeQuery(sql);
+
+            // Manage the result in a bean
+            while(resultset.next())
+            {
+                // Manage the result in a list of bean
+                long promitionId = resultset.getLong("promotion_id");
+                int type = resultset.getInt("type_promotion");
+                int game = resultset.getInt("game");
+                int multiplier = resultset.getInt("multiplier");
+                String message = resultset.getString("message");
+                Timestamp startDate = resultset.getTimestamp("start_date");
+                Timestamp endDate = resultset.getTimestamp("end_date");
+                PromotionsBean promotionsBean = new PromotionsBean(promitionId, type, game, multiplier, message, startDate, endDate);
+                promotionList.add(promotionsBean);
+            }
+            return promotionList;
+        }
+        catch(SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            // Close the query environment in order to prevent leaks
+            close();
+        }
+        return null;
+    }
+
+    // Create a promotion
+    public void createPromotion(PromotionsBean promotionsBean, DataSource dataSource) {
+        try {
+            // Set connection
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+
+            // Query construction
+            String sql = "";
+            sql += "insert into promotions(type_promotion, game, multiplier, message, start_date, end_date)";
+            sql += " values (" + promotionsBean.getTypePromotion();
+            sql += ", " + promotionsBean.getGame() + "";
+            sql += ", " + promotionsBean.getMultiplier();
+            sql += ", '" + promotionsBean.getMessage() + "'";
+            sql += ", '" + promotionsBean.getStartDate() + "'";
+            sql += ", '" + promotionsBean.getEndDate() + "')";
+
+            // Execute the query
+            statement.executeUpdate(sql);
+        }
+        catch (SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            // Close the query environment in order to prevent leaks
+            close();
+        }
+    }
+
+
+    // Delete a promotion
+    public void deletePromotion(PromotionsBean promotionsBean, DataSource dataSource)
+    {
+        try
+        {
+            // Set connection
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+
+            // Query construction
+            String sql = "";
+            sql += "delete from promotions where promotion_id=" + promotionsBean.getPromotionId();
+
+            // Execute the query
+            statement.executeUpdate(sql);
+        }
+        catch(SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            // Close the query environment in order to prevent leaks
+            close();
+        }
+    }
+
+
+    // Close all connection
+    public void close()
+    {
+        // Close the query environment in order to prevent leaks
+        try
+        {
+            if (resultset != null)
+            {
+                // Close the resulset
+                resultset.close();
+            }
+            if (statement != null)
+            {
+                // Close the statement
+                statement.close();
+            }
+            if (connection != null)
+            {
+                // Close the connection
+                connection.close();
+            }
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
 }
