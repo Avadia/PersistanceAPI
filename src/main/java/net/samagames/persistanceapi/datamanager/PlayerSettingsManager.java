96a7cc785566c859f230afba00fe1587040a90ef
Jacky Revet <Jacky@Mac-Pro-de-Jacky.local>
2016-03-17 21:50:02 +0100

    Ajout des requêtes SQL pour la gestion des settings

--- src/main/java/net/samagames/persistanceapi/datamanager/PlayerSettingsManager.java
+++ src/main/java/net/samagames/persistanceapi/datamanager/PlayerSettingsManager.java
@@ -45,7 +45,9 @@ public class PlayerSettingsManager
 
             // Query construction
             String sql = "";
-            sql += "";
+            sql += "select (HEX(player_uuid)) as uuid, jukebox_listen, group_demand_receive, friendship_demand_receive, notification_receive, private_message_receive, chat_visible, player_visible";
+            sql += ", waiting_line_notification, other_player_interaction, click_on_me_activation, allow_statistic_onclick, allow_coins_onclick, allow_stars_onclick, allow_click_on_other";
+            sql += " from player_settings where player_uuid=(UNHEX('" + Transcoder.Encode(player.getUuid().toString()) + "'))";
 
             // Execute the query
             resultset = statement.executeQuery(sql);
@@ -55,25 +57,32 @@ public class PlayerSettingsManager
             {
                 // There's a result
                 String playerUuid = Transcoder.Decode(resultset.getString("uuid"));
-                boolean jukeboxListen = resultset.getBoolean("");
-                boolean groupDemandReceive = resultset.getBoolean("");
-                boolean friendshipDemandReceive =  resultset.getBoolean("");
-                boolean notificationReceive = resultset.getBoolean("");
-                boolean privateMessageReceive = resultset.getBoolean("");
-                boolean chatVisible = resultset.getBoolean("");
-                boolean playerVisible = resultset.getBoolean("");
-                boolean waitingLineNotification = resultset.getBoolean("");
-                boolean otherPlayerInteraction = resultset.getBoolean("");
-                boolean clickOnMeActivation = resultset.getBoolean("");
-                boolean allowStatisticOnClick = resultset.getBoolean("");
-                boolean allowCoinsOnClick = resultset.getBoolean("");
-                boolean allowStarsOnclick = resultset.getBoolean("");
-                boolean allowClickOnOther = resultset.getBoolean("");
-                //playerSettingsBean = new PlayerSettingsBean();
+                UUID uuid = UUID.fromString(playerUuid);
+                boolean jukeboxListen = resultset.getBoolean("jukebox_listen");
+                boolean groupDemandReceive = resultset.getBoolean("group_demand_receive");
+                boolean friendshipDemandReceive =  resultset.getBoolean("friendship_demand_receive");
+                boolean notificationReceive = resultset.getBoolean("notification_receive");
+                boolean privateMessageReceive = resultset.getBoolean("private_message_receive");
+                boolean chatVisible = resultset.getBoolean("chat_visible");
+                boolean playerVisible = resultset.getBoolean("player_visible");
+                boolean waitingLineNotification = resultset.getBoolean("waiting_line_notification");
+                boolean otherPlayerInteraction = resultset.getBoolean("other_player_interaction");
+                boolean clickOnMeActivation = resultset.getBoolean("click_on_me_activation");
+                boolean allowStatisticOnClick = resultset.getBoolean("allow_statistic_onclick");
+                boolean allowCoinsOnClick = resultset.getBoolean("allow_coins_onclick");
+                boolean allowStarsOnclick = resultset.getBoolean("allow_stars_onclick");
+                boolean allowClickOnOther = resultset.getBoolean("allow_click_on_other");
+                playerSettingsBean = new PlayerSettingsBean(uuid, jukeboxListen, groupDemandReceive, friendshipDemandReceive, notificationReceive, privateMessageReceive, chatVisible,
+                        playerVisible, waitingLineNotification, otherPlayerInteraction, clickOnMeActivation, allowStatisticOnClick, allowCoinsOnClick, allowStarsOnclick, allowClickOnOther);
                 return playerSettingsBean;
             }
             else
             {
+                // If there no player settings for the uuid in database create a new player settings
+                this.close();
+                this.createDefaultPlayerSettings(player, dataSource);
+                PlayerSettingsBean settings = this.getPlayerSettings(player, dataSource);
+                this.close();
                 return playerSettingsBean;
             }
         }
@@ -90,7 +99,7 @@ public class PlayerSettingsManager
     }
 
     // Set player ingame settings
-    public void setPlayerSettings(PlayerSettingsBean player, PlayerSettingsBean settingsBeans, DataSource dataSource)
+    public void setPlayerSettings(PlayerBean player, PlayerSettingsBean settingsBeans, DataSource dataSource)
     {
         // Update the players data
         try
@@ -101,23 +110,21 @@ public class PlayerSettingsManager
 
             // Query construction
             String sql = "";
-            sql += "update player_settings set (jukeboxListen, groupDemandReceive, friendshipDemandReceive, notificationReceive, privateMessageReceive, chatVisible, playerVisible ";
-            sql += ", waitingLineNotification, otherPlayerInteraction, clickOnMeActivation, allowStatisticOnClick, allowCoinsOnClick, allowStarsOnclick, allowClickOnOther) set values(";
-            sql += ", " + settingsBeans.isJukeboxListen();
-            sql += ", " + settingsBeans.isGroupDemandReceive();
-            sql += ", " + settingsBeans.isFriendshipDemandReceive();
-            sql += ", " + settingsBeans.isNotificationReceive();
-            sql += ", " + settingsBeans.isPrivateMessageReceive();
-            sql += ", " + settingsBeans.isChatVisible();
-            sql += ", " + settingsBeans.isPlayerVisible();
-            sql += ", " + settingsBeans.isWaitingLineNotification();
-            sql += ", " + settingsBeans.isOtherPlayerInteraction();
-            sql += ", " + settingsBeans.isClickOnMeActivation();
-            sql += ", " + settingsBeans.isAllowStatisticOnClick();
-            sql += ", " + settingsBeans.isAllowCoinsOnClick();
-            sql += ", " + settingsBeans.isAllowStarsOnclick();
-            sql += ", " + settingsBeans.isAllowClickOnOther();
-            sql += " where uuid=(UNHEX('" + Transcoder.Encode(player.getUuid().toString()) + "'))";
+            sql += "update player_settings set jukebox_listen=" + settingsBeans.isJukeboxListen();
+            sql += ", group_demand_receive=" +  settingsBeans.isGroupDemandReceive();
+            sql += ", friendship_demand_receive=" + settingsBeans.isFriendshipDemandReceive();
+            sql += ", notification_receive=" + settingsBeans.isNotificationReceive();
+            sql += ", private_message_receive=" + settingsBeans.isPrivateMessageReceive();
+            sql += ", private_message_receive=" + settingsBeans.isChatVisible();
+            sql += ", chat_visible=" + settingsBeans.isPlayerVisible();
+            sql += ", player_visible=" + settingsBeans.isWaitingLineNotification();
+            sql += ", other_player_interaction=" + settingsBeans.isOtherPlayerInteraction();
+            sql += ", click_on_me_activation=" + settingsBeans.isClickOnMeActivation();
+            sql += ", allow_statistic_onclick=" + settingsBeans.isAllowStatisticOnClick();
+            sql += ", allow_coins_onclick=" + settingsBeans.isAllowCoinsOnClick();
+            sql += ", allow_stars_onclick=" + settingsBeans.isAllowStarsOnclick();
+            sql += ", allow_click_on_other=" + settingsBeans.isAllowClickOnOther();
+             sql += " where player_uuid=(UNHEX('" + Transcoder.Encode(player.getUuid().toString()) + "'))";
 
             // Execute the query
             statement.executeUpdate(sql);
@@ -133,6 +140,35 @@ public class PlayerSettingsManager
         }
     }
 
+    // Create default player settings
+    public void createDefaultPlayerSettings(PlayerBean player, DataSource dataSource)
+    {
+        try
+        {
+            // Set connection
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+
+            // Query construction
+            String sql = "insert into player_settings (player_uuid, jukebox_listen, group_demand_receive, friendship_demand_receive, notification_receive, private_message_receive";
+            sql += ", chat_visible, player_visible, waiting_line_notification, other_player_interaction, click_on_me_activation, allow_statistic_onclick, allow_coins_onclick";
+            sql += ", allow_stars_onclick, allow_click_on_other)";
+            sql += " values (UNHEX('"+ Transcoder.Encode(player.getUuid().toString())+"')";
+            sql += ", true, true, true, true, true, true, true, true, true, true, true, true, true, true)";
+
+            // Execute the query
+            statement.executeUpdate(sql);
+        }
+        catch (SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            // Close the query environment in order to prevent leaks
+            close();
+        }
+    }
 
     // Close the connection
     public void close()
