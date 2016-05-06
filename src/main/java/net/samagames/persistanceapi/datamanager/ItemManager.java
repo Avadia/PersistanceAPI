0eb0ecb758dc3f48d87e2d748fcd2954424ba90d
Silvanosky <geekpower14@gmail.com>
2016-05-06 18:35:41 +0200

    Fix type of variables

--- src/main/java/net/samagames/persistanceapi/datamanager/ItemManager.java
+++ src/main/java/net/samagames/persistanceapi/datamanager/ItemManager.java
@@ -63,8 +63,8 @@ public class ItemManager
                 int priceStars = resultset.getInt("price_stars");
                 int gameCategory = resultset.getInt("game_category");
                 String itemMinecraftId = resultset.getString("item_minecraft_id");
-                String itemRarity = resultset.getString("item_rarity");
-                int rankAccessiblity = resultset.getInt("rankAccessibility");
+                int itemRarity = resultset.getInt("item_rarity");
+                String rankAccessiblity = resultset.getString("rankAccessibility");
                 itemDescription = new ItemDescriptionBean(itemId, itemName, itemDesc, priceCoins, priceStars, gameCategory, itemMinecraftId, itemRarity, rankAccessiblity);
 
             }
@@ -113,8 +113,8 @@ public class ItemManager
                 int priceStars = resultset.getInt("price_stars");
                 int gameCategory = resultset.getInt("game_category");
                 String itemMinecraftId = resultset.getString("item_minecraft_id");
-                String itemRarity = resultset.getString("item_rarity");
-                int rankAccessiblity = resultset.getInt("rankAccessibility");
+                int itemRarity = resultset.getInt("item_rarity");
+                String rankAccessiblity = resultset.getString("rankAccessibility");
                 itemDescription = new ItemDescriptionBean(itemId, itemName, itemDesc, priceCoins, priceStars, gameCategory, itemMinecraftId, itemRarity, rankAccessiblity);
                 itemList.add(itemDescription);
             }
